4607fb1185eb78b310096e920d59adbed2b9acf4
tomasteylor25 <tomasteylor.24@gmail.com>
2026-08-13 15:55:33 -0500

    Se agrega componente home con avances de este.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { HomeComponent } from './pages/home/home';
+
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: '',
