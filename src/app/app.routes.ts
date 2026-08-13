f4daaa45bd70bfba2e8276f56fc634127d31cdd5
MIGUEL200401 <miguelperez.di2004@gmail.com>
2026-08-13 16:02:22 -0500

    se creo componente perfil con avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { PerfilComponent } from './pages/perfil/perfil';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
   },
   {
     path: '',
