55e49d9945cf0cb31a30bb331c89d71c37a9fd06
brayan2025703 <brayanstivene.a.04@gmail.com>
2026-08-13 16:29:38 -0500

    se crean componentes de formularios con sus avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Formulario_crear_trueques } from './pages/formulario-crear-trueques/formulario_crear_trueques';
+import { Formulario_crear_usuarioComponent } from './pages/formulario_crear_usuario/formulario_crear_usuario';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'formulario',
+    component: Formulario_crear_usuarioComponent,
+  },
+  {
+    path: 'trueque',
+    component: Formulario_crear_trueques
   },
   {
     path: '',
