02f9e946e30c1488bb71517952e58e6cc177d312
brayan2025703 <brayanstivene.a.04@gmail.com>
2026-08-18 17:48:11 -0500

    se hace actualizancion en las rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,57 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { HomeComponent } from './pages/home/home';
+import { ModalDetalleTruequeComponent } from './layout/modal-detalle-trueque/ModalDetalleTrueque';
+import { PerfilComponent } from './pages/perfil/perfil';
+import { InformacionComponent } from './pages/informacion/informacion';
+import { SeguridadComponent } from './pages/seguridad/seguridad';
+import { LegalInfo } from './pages/legal-info/legal-info';
 import { Formulario_crear_truequesComponent } from './pages/formulario-crear-trueques/formulario_crear_trueques';
 import { Formulario_crear_usuarioComponent } from './pages/formulario_crear_usuario/formulario_crear_usuario';
 
 export const routes: Routes = [
   {
     path: 'login',
-    component: Login
+    component: Login,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'trueque/:id',
+    component: ModalDetalleTruequeComponent,
+  },
+  {
+    path: 'trueque',
+    component: Formulario_crear_truequesComponent,
   },
   {
     path: 'formulario',
     component: Formulario_crear_usuarioComponent,
   },
   {
-    path: 'trueque',
-    component: Formulario_crear_truequesComponent
+    path: 'legal-info',
+    component: LegalInfo,
+  },
+  // PERFIL
+  {
+    path: 'perfil',
+    component: PerfilComponent,
+  },
+  // PERFIL > TU INFORMACIÓN
+  {
+    path: 'perfil/informacion',
+    component: InformacionComponent,
+  },
+  // PERFIL > SEGURIDAD
+  {
+    path: 'perfil/seguridad',
+    component: SeguridadComponent,
   },
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
