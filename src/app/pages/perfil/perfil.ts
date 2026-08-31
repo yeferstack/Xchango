823596c6c45ef1fe89b2c522bc526434fb823ed4
tomasteylor25 <tomasteylor.24@gmail.com>
2026-08-31 15:01:15 -0500

    se realizan cambios en imagenes de las publicaciones.json y se modifica icono de la pagina

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -31,7 +31,7 @@ export class PerfilComponent implements OnInit {
   }
 
   get avatarUrl(): string {
-    return this.servicio.usuarioActual()?.avatar ?? 'Logo-xchango/avatar-miguel.jpeg';
+    return this.servicio.usuarioActual()?.avatar ?? 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   }
 
   get ubicacion(): string {
